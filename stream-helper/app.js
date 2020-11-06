9d43fbd38913538ffb7577c3dec0e40dacf0d448
Jonas Knudsen <jonas.a.knudsen@gmail.com>
2020-11-06 15:11:45 -0700

    Initial commit

---
+++ stream-helper/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+// const mongoLoader = require('./mongo-loader');
+const api = require('./api');
+
+// make an express app
+const app = express();
+const port = 3000;
+
+// always load mongo
+// mongoLoader.loadMongo().then(() => console.log("Done opening MongoDB"));
+
+// get the API routing started
+app.use('/api', api);
+
+// use the static directory to serve static html files
+app.use(express.static('static'));
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
