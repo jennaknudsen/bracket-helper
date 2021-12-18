2be65a991d66eb8bb455b9388b05b294307b89e6
Jonas Knudsen <jonas.a.knudsen@gmail.com>
2021-12-18 15:07:10 -0700

    More work on API

--- stream-helper/api.js
+++ stream-helper/api.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const fs = require('fs');
+
+const text_dir_name = 'text_files/';
+const round_filename = 'round_name.txt';
+const p1_filename = 'p1_name.txt';
+const p1_score_filename = 'p1_score.txt';
+const p2_filename = 'p2_name.txt';
+const p2_score_filename = 'p2_score.txt';
+
+let last_values = {
+    round: "",
+    p1_name: "",
+    p1_losers: false,
+    p1_score: 0,
+    p2_name: "",
+    p2_score: 0,
+    p2_losers: true,
+}
 
 const router = express.Router();
  
@@ -15,7 +33,46 @@ router.put('/', jsonParser, (req, res) => {
     res.set('Access-Control-Allow-Methods', 'POST');
     res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-    res.send({'test': 'success'});
+    const thisRound = req.body.round;
+    const thisP1Name = req.body.p1_name;
+    const thisP1Losers = req.body.p1_losers;
+    const thisP1Score = req.body.p1_score;
+    const thisP2Name = req.body.p2_name;
+    const thisP2Losers = req.body.p2_losers;
+    const thisP2Score = req.body.p2_score;
+
+    // if any different values, write them
+    const folderName = __dirname + '/' + text_dir_name;
+
+    if (thisRound !== last_values.round) {
+        fs.writeFileSync(folderName + round_filename, thisRound, {flag: 'w'}, err => console.error(err));
+    }
+    if (thisP1Name !== last_values.p1_name || thisP1Losers !== last_values.p1_score) {
+        const name = thisP1Name + (thisP1Losers ? " (L)" : "");
+        fs.writeFileSync(folderName + p1_filename, name, {flag: 'w'}, err => console.error(err));
+    }
+    if (thisP1Score !== last_values.p1_score) {
+        fs.writeFileSync(folderName + p1_score_filename, thisP1Score, {flag: 'w'}, err => console.error(err));
+    }
+    if (thisP2Name !== last_values.p2_name || thisP2Losers !== last_values.p2_score) {
+        const name = thisP1Name + (thisP2Losers ? " (L)" : "");
+        fs.writeFileSync(folderName + p2_filename, name, {flag: 'w'}, err => console.error(err));
+    }
+    if (thisP1Score !== last_values.p2_score) {
+        fs.writeFileSync(folderName + p2_score_filename, thisP1Score, {flag: 'w'}, err => console.error(err));
+    }
+
+    // update the last values
+    last_values = {
+        round: thisRound,
+        p1_name: thisP1Name,
+        p1_losers: thisP1Losers,
+        p1_score: thisP1Score,
+        p2_name: thisP2Name,
+        p2_score: thisP2Score,
+        p2_losers: thisP2Losers,
+    }
+
 });
 
 module.exports = router;
