8af7fc2972f01ec28897850ab85e12a4b0d8f5cc
Jonas Knudsen <jonas.a.knudsen@gmail.com>
2021-12-18 14:06:20 -0700

    Got events firing

--- stream-helper/static/script.js
+++ stream-helper/static/script.js
@@ -1,26 +1,45 @@
+let lastChangeTime = 0;
+const timeToUpdate = 2000;
+
 function textChanged() {
-    console.log("Text changed");
+    const currentTime = Date.now();
+    lastChangeTime = currentTime;
+
+    setTimeout(() => {
+        if (currentTime === lastChangeTime) {
+            console.log("Event fired");
+            // SEND REQUEST HERE
+        }
+    }, timeToUpdate);
 }
 
-// const p1_score = document.getElementById('player-1-score');
-const p2_score = document.getElementById('player-2-score');
-
+/**
+ * These next four functions control the plus / minus on the interface
+ */
 function decrementP1() {
     const p1_score = document.getElementById('player-1-score');
-    if (parseInt(p1_score.value) !== 0) p1_score.value = parseInt(p1_score.value) - 1;
+    if (parseInt(p1_score.value) !== 0) { 
+        p1_score.value = parseInt(p1_score.value) - 1;
+        textChanged();
+    }
 }
 
 function incrementP1() {
     const p1_score = document.getElementById('player-1-score');
     p1_score.value = parseInt(p1_score.value) + 1;
+    textChanged();
 }
 
 function decrementP2() {
     const p2_score = document.getElementById('player-2-score');
-    if (parseInt(p2_score.value) !== 0) p2_score.value = parseInt(p2_score.value) - 1;
+    if (parseInt(p2_score.value) !== 0) {
+        p2_score.value = parseInt(p2_score.value) - 1;
+        textChanged();
+    }
 }
 
 function incrementP2() {
     const p2_score = document.getElementById('player-2-score');
     p2_score.value = parseInt(p2_score.value) + 1;
+    textChanged();
 }
